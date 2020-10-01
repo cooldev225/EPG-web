a1f86b2a9aa144d8357af9453bbecac710fed62c
developer225 <developer225@hotmail.com>
2020-10-02 01:09:54 +0900

    Create Login.php

---
+++ application/modules/admin/controllers/Login.php
@@ -0,0 +1,62 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// NOTE: this controller inherits from MY_Controller instead of Admin_Controller,
+// since no authentication is required
+class Login extends MY_Controller {
+
+	/**
+	 * Login page and submission
+	 */
+	public function index()
+	{
+		$this->load->library('form_builder');
+		$form = $this->form_builder->create_form();
+
+		if (!empty($this->input->post('username')))//$form->validate())
+		{
+			// passed validation
+			$identity = $this->input->post('username');
+			$password = $this->input->post('password');
+			$remember = ($this->input->post('remember')=='on');
+			
+			$this->distroy_session();
+			
+			if ($this->ion_auth->login($identity, $password, $remember))
+			{
+				// login succeed
+				$messages = $this->ion_auth->messages();
+				$this->system_message->set_success($messages);
+				redirect($this->mModule);
+			}
+			else
+			{
+				// login failed
+				$errors = $this->ion_auth->errors();
+				$this->system_message->set_error($errors);
+				refresh();
+			}
+		}
+		
+		// display form when no POST data, or validation failed
+		$this->mViewData['form'] = $form;
+		$this->mBodyClass = 'login-page';
+		$this->render('login', 'empty');
+	}
+	public function distroy_session(){
+		// Destroy the session
+		$this->session->sess_destroy();
+		//Recreate the session
+		if (substr(CI_VERSION, 0, 1) == '2')
+		{
+			$this->session->sess_create();
+		}
+		else
+		{
+			if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
+				session_start();
+			}
+			$this->session->sess_regenerate(TRUE);
+		}
+	}
+}
