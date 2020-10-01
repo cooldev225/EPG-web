16b8b51b1bae635187a30be533c1bfd0fbb18d4e
developer225 <developer225@hotmail.com>
2020-10-02 02:33:10 +0900

    Update info.php

--- info.php
+++ info.php
@@ -1,1 +1,12 @@
-<?php echo phpinfo();?>
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "root";
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+echo phpinfo();?>
